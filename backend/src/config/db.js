39bcea273c59f5c4b15547be91d809d16c4096fb
Kaif <smdkaiffaraz@gmail.com>
2026-05-26 18:08:31 +0530

    connected railway database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -12,7 +12,7 @@ db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err);
   } else {
-    console.log("MySQL Connected");
+    console.log("MySQL Connected Successfully");
   }
 });
 
