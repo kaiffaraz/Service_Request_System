e83bd482cd1bf4c943866018312f3d6ca60545ab
Kaif <smdkaiffaraz@gmail.com>
2026-05-27 01:25:17 +0530

    fixed ES module support

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2");
+import mysql from "mysql2";
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -16,4 +16,4 @@ db.connect((err) => {
   }
 });
 
-module.exports = db;
+export default db;
